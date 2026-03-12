fdaf04ca99f3b86913c9961dd5a2dc38fd62d104
Jacob ROva <87586912+RovaNapster@users.noreply.github.com>
2026-03-12 06:29:19 +0100

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,16 @@
+{
+  "name": "Rova Ketchup Pro",
+  "short_name": "Rova",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#000000",
+  "theme_color": "#000000",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "https://emojigraph.org/media/apple/tomato_1f345.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ]
+}
